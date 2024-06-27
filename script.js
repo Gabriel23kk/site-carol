afc8632dabc06e6ebd4306b94a636db61c4917d0
Gabriel23kk <ps996489781@gmail.com>
2024-06-27 14:20:23 -0300

    meio do projeto

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 
 const img = document.querySelectorAll(".image-1");
 const img2 = document.querySelector("#image-s")
+const img3 = document.querySelector("#img1")
+const img4 = document.querySelector("#img11")
 let blur = document.getElementById('blur');
 let box = document.getElementById("aparecer");
-let imgalter = document.querySelector("#imgalter");
+let imgalter = document.getElementById("imgalter")
 function toggle(){
     
     blur.classList.add('active');
     box.classList.add('ativo');
-    for (let index = 0; index < imggg.length; index++) {
-        imgalter.src = imggg[index];
-        break
-    }
+    imgalter.src = "./assets/1.png"
+    
 }
-
-let imggg = ["./assets/1.png",
-             "./assets/2.png",
-             "./assets/3.png" , 
-             "./assets/4.png"]
-
+function imgS(){
+    blur.classList.add('active');
+    box.classList.add('ativo');
+    imgalter.src = "./assets/2.png"
+}
+function imgL(){
+    blur.classList.add('active');
+    box.classList.add('ativo');
+    imgalter.src = "./assets/3.png"
+}
+function imgR(){
+    blur.classList.add('active');
+    box.classList.add('ativo');
+    imgalter.src = "./assets/4.jpg"
+}
 const close = document.querySelector('.close')
 
 function fechar(){
